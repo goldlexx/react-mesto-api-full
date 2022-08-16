f3dfe766e88012a593637c52ffe39cc4acab834d
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-08-16 14:24:09 +0300

    add crash test

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,12 @@ app.use(requestLogger);
 
 app.use(cors());
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post(
   '/signin',
   celebrate({
