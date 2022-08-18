c37d7cb13317868b6a15abb396404f76fb747dfc
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-08-18 22:15:36 +0300

    Fix replace

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -3,7 +3,7 @@ const { ErrorNotFound, Forbidden } = require('../errors/allErrors');
 const { errorMessage } = require('../utils/errorMessage');
 
 module.exports.getCards = (req, res, next) => {
-  Card.find({}).sort({ createdAt: 1 })
+  Card.find({}).sort({ createdAt: -1 })
     .then((cards) => res.send(cards))
     .catch(next);
 };
